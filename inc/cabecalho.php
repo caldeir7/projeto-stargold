fbff8dea1213ecd904d5a58fc38d1a99baddef3b
HttpCaldeira <103135145+caldeir7@users.noreply.github.com>
2022-06-14 11:40:01 -0300

    att css

--- inc/cabecalho.php
+++ inc/cabecalho.php
@@ -6,9 +6,12 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title> Microblog </title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+  
+
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="css/style.css">
+  
+ <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
@@ -22,7 +25,7 @@
           <a class="../index.php" href="index.php">StarGold</a>
         </h1>
 
-        <div class="god-links" id="navbarSupportedContent">
+        <div class="god-links" id="">
           <h2><a class="icones" href=""><i class="fa fa-bars"></i></a></h2>
           <ul class="links-menu aberto">
             <li class="">
